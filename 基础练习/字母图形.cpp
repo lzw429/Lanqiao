4c567e0a4bfde20dd5975f2ce666d9f3e78468ea
Yiheng Shu <lzw429@qq.com>
2018-03-12 22:24:36 +0800

    update

--- 基础练习/字母图形.cpp
+++ 基础练习/字母图形.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,8 +7,12 @@ int main() {
     int n, m;
     cin >> n >> m;
     int i, j;
-    for (i = 0; i < m; i++) {
-
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            int delta = abs(i - j);
+            cout << (char) ('A' + delta);
+        }
+        cout << endl;
     }
     return 0;
 }
