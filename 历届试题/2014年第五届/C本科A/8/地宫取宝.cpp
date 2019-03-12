4077d252f82b59250805671f5ec6576975f9cb9c
Yiheng Shu <lzw429@qq.com>
2019-03-12 21:57:37 +0800

    update

--- 历届试题/2014年第五届/C本科A/8/地宫取宝.cpp
+++ 历届试题/2014年第五届/C本科A/8/地宫取宝.cpp
@@ -1,6 +1,65 @@
-#include <iostream>
+#include<iostream>
+#include<cstring>
+
+#define MAXN 60
+#define MOD 1000000007
 using namespace std;
 
+int n, m, k; // 地图n行m列，最多k件宝贝
+int map[MAXN][MAXN];
+int dp[MAXN][MAXN][15][15];
+
+// 记忆化DFS
+int DFS(int x, int y, int sum, int max) {
+    // (x, y)表示坐标；sum是手中宝贝件数，max是手中宝贝最大价值
+    // 返回行动方案数
+    int ret = 0;
+    if (dp[x][y][sum][max + 1] != -1) // max+1 是因为max初始值为-1，下同
+    {
+        return dp[x][y][sum][max + 1];
+    }
+    
+    int cur = map[x][y];
+    if (x == n - 1 && y == m - 1) {
+        // 深搜到终点
+        if (sum == k || (cur > max && sum == k - 1))
+            ret++;
+        ret %= MOD; 
+        return ret;
+    }
+    
+    // 向右走
+    if (x < n - 1) {
+        if (cur > max) {
+            // 此地宝贝价值大于手中任意宝贝价值，可以获得该宝贝，也可放弃
+            ret += DFS(x + 1, y, sum + 1, cur);
+            ret %= MOD;
+        }
+        ret += DFS(x + 1, y, sum, max);
+        ret %= MOD;
+    }
+
+    // 向下走
+    if (y < m - 1) {
+        if (cur > max) {
+            ret += DFS(x, y + 1, sum + 1, cur);
+            ret %= MOD;
+        }
+        ret += DFS(x, y + 1, sum, max);
+        ret %= MOD;
+    }
+    dp[x][y][sum][max + 1] = ret;
+    return ret;
+}
+
 int main() {
+    memset(dp, -1, sizeof(dp)); // 这步不可忽略 
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; ++i) { // x范围[0, n-1]
+        for (int j = 0; j < m; ++j) { // y范围[0, m-1]
+            cin >> map[i][j];
+        }
+    }
+    cout << DFS(0, 0, 0, -1);
     return 0;
 }
---
+++ 历届试题/2014年第五届/C本科A/9/斐波那契.cpp
@@ -0,0 +1,80 @@
+#include <climits>
+#include <iostream>
+#include <cstdio> 
+#include <vector>
+
+using namespace std;
+typedef vector<long long> vec;
+typedef vector <vec> mat;
+
+long long n, m, p, MOD;
+
+long long mul(long long a, long long b) {
+    if (a > b) {
+        long long t = a;
+        a = b;
+        b = t;
+    }
+    long long ans = 0; // a * b % MOD
+    while (b) {
+        if(b & 1)
+            ans = (ans + a) % MOD;
+        a = (a + a) % MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+mat mul(const mat &A, const mat &B) {
+    mat C(A.size(), vec(B[0].size()));
+    for (int i = 0; i < A.size(); i++) {
+        for (int k = 0; k < B.size(); k++) {
+            for (int j = 0; j < B[0].size(); j++) {
+                C[i][j] = (C[i][j] + mul(A[i][k], B[k][j])) % MOD;
+            }
+        }
+    }
+    return C;
+}
+
+mat pow(mat A, long long n) {
+    mat An(A.size(), vec(A.size()));
+    for (int i = 0; i < A.size(); i++)
+        An[i][i] = 1;
+    while (n > 0) {
+        if (n & 1)
+            An = mul(A, An);
+        A = mul(A, A);
+        n >>= 1;
+    }
+    return An;
+}
+
+mat fibInit() {
+    mat Fib(2, vec(2));
+    Fib[0][0] = 1;
+    Fib[0][1] = 1;
+    Fib[1][0] = 1;
+    Fib[1][1] = 0;
+    return Fib;
+}
+
+long long getFib(long long n) {
+    mat fib = fibInit(); 
+    mat ans = pow(fib, n);
+    return ans[0][1];
+}
+
+int main() {
+    cin >> n >> m >> p;
+    MOD = p;
+    if(m >= n + 2) {
+        cout << getFib(n + 2) - 1;
+    } else {
+        MOD = LLONG_MAX;
+        MOD = getFib(m); 
+        cout << getFib(n + 2) % p - 1;
+    } 
+    return 0;
+}
+
