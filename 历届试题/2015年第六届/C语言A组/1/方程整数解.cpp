680ccfc60cb60c0ea717e1e69c19f1d242ab54aa
Yiheng Shu <lzw429@qq.com>
2019-03-13 17:21:12 +0800

    update

--- 历届试题/2015年第六届/C语言A组/1/方程整数解.cpp
+++ 历届试题/2015年第六届/C语言A组/1/方程整数解.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            for (int k = 0; k < 32; k++) {
+    for (int i = 1; i < 32; i++) {
+        for (int j = 1; j < 32; j++) {
+            for (int k = 1; k < 32; k++) {
                 if (j != i && k != i && k != j)
                 if (i * i + j * j + k * k == 1000) {
                     cout << i << " " << j << " " << k << " " << endl;
--- 历届试题/2015年第六届/C语言A组/3/奇妙的数字.cpp
+++ 历届试题/2015年第六届/C语言A组/3/奇妙的数字.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
 using namespace std;
+
+string intToStr(int num) {
+    stringstream ss;
+    ss << num;
+    string res;
+    ss >> res;
+    return res;
+}
+
+bool check(int i2, int i3) {
+    string i2str = intToStr(i2);
+    string i3str = intToStr(i3);
+    if(i2str.size() + i3str.size() != 10) return false;
+    string concat = i2str + i3str;
+    sort(concat.begin(), concat.end());
+    for(int i = 1; i < concat.size(); i++) {
+        if(concat[i] == concat[i - 1])
+            return false;
+    }
+    return true;
+}
 
 int main() {
     for (int i = 0; i < 10000; i++) {
+         int i2 = i * i;
+         int i3 = i2 * i;
+         if(check(i2, i3))
+            cout << i << " " << i2 << " " << i3 << endl;
     }
     return 0;
 }
